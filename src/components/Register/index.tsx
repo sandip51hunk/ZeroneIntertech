87b6a236b22cead7dfb98b0d5eb7aea0436533c5
anish khanal <anish.khanal@amniltech.com>
2022-09-13 21:35:58 +0545

    work on register page

--- src/components/Register/index.tsx
+++ src/components/Register/index.tsx
@@ -3,39 +3,38 @@ import { useNavigate } from 'react-router-dom'
 import { Header } from '../Navigation/Header/Header'
 
 function Register() {
-  
+
   const navigate = useNavigate();
-  
+
   return (
     <>
       <Header />
       <section className="text-gray-600 flex justify-center items-center body-font relative">
         <div>
-          <div className='bg-secondary flex gap-20 mt-28'>
-          
-            <div className='py-2 px-4 border-solid border-2 rounded'>1</div>
+          <div className='flex gap-20 mt-24 mb-8 items-center '>
+            <div className='py-2 px-4 border-solid border-2 rounded cursor-pointer'>1</div>
             <span>sdasd</span>
-            <div>2</div>
+            <div className='py-2 px-4 border-solid border-2 rounded cursor-pointer'>2</div>
             <span>sdasda</span>
-            <div>3</div>
+            <div className='py-2 px-4 border-solid border-2 rounded cursor-pointer'>3</div>
             <span>sdasd</span>
-            <div>4</div>
+            <div className='py-2 px-4 border-solid border-2 rounded cursor-pointer'>4</div>
           </div>
           <form className='p-10 border-solid border-2'>
-          <h1 className='title-font text-3xl font-bold pb-2'>Register</h1>
-            <p className='pb-2 text-base'>Enter your mobile number to continue.</p>
+            <h1 className='title-font text-3xl font-bold mb-2'>Register</h1>
+            <p className='mb-7 text-base'>Enter your mobile number to continue.</p>
             <div className="relative mb-4">
               <label htmlFor="number" className="">
                 Mobile Number
-              </label>
+              </label><br />
               <input
-                className=''
+                className='p-1.5 w-full border mb-7 rounded'
                 type="text"
-              />
-              <button className="">
+              /><br />
+              <button className="mb-5 p-1.5 w-full border rounded">
                 Continue
               </button>
-              <p>Already have an account?<a onClick={() => navigate('/login')}> Login</a></p>
+              <p>Already have an account?<a className='cursor-pointer text-red-500' onClick={() => navigate('/login')}> Login</a></p>
             </div>
           </form>
         </div>
