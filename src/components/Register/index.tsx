d25b64a6fde85be3b4a51af4d5ae9351a8aa30cd
anish khanal <anish.khanal@amniltech.com>
2022-09-13 17:08:57 +0545

    register page and inter font changed

--- src/components/Register/index.tsx
+++ src/components/Register/index.tsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Header } from '../Navigation/Header/Header'
 
 function Register() {
+  
+  const navigate = useNavigate();
+  
   return (
-    <div>Register</div>
+    <>
+      <Header />
+      <section className="text-gray-600 flex justify-center items-center body-font relative">
+        <div>
+          <div className='bg-secondary flex gap-20 mt-28'>
+          
+            <div className='py-2 px-4 border-solid border-2 rounded'>1</div>
+            <span>sdasd</span>
+            <div>2</div>
+            <span>sdasda</span>
+            <div>3</div>
+            <span>sdasd</span>
+            <div>4</div>
+          </div>
+          <form className='p-10 border-solid border-2'>
+          <h1 className='title-font text-3xl font-bold pb-2'>Register</h1>
+            <p className='pb-2 text-base'>Enter your mobile number to continue.</p>
+            <div className="relative mb-4">
+              <label htmlFor="number" className="">
+                Mobile Number
+              </label>
+              <input
+                className=''
+                type="text"
+              />
+              <button className="">
+                Continue
+              </button>
+              <p>Already have an account?<a onClick={() => navigate('/login')}> Login</a></p>
+            </div>
+          </form>
+        </div>
+      </section>
+    </>
   )
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      'secondary': '#EC4A47',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
